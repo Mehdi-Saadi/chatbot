64aac2a076f3a0d3bf5471055493fd560d929c85
Mehdi <106740682+Mehdi-Saadi@users.noreply.github.com>
2023-05-18 15:22:23 +0330

    Add files via upload

---
+++ chatbot/db/response.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 18, 2023 at 01:51 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `chatbot`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `response`
+--
+
+CREATE TABLE `response` (
+  `id` int(11) NOT NULL,
+  `message` varchar(255) NOT NULL,
+  `response` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `response`
+--
+
+INSERT INTO `response` (`id`, `message`, `response`) VALUES
+(1, 'hi|hey|hy|hello|hay', 'Hi, how can i help you?'),
+(2, 'how are you|how r u|how are u', 'I am fine, thank you for asking.'),
+(3, 'who are you|who r u|who are u', 'I am a chatbot created by mehdi.'),
+(4, 'who is mehdi', 'Mehdi Saadi is a programmer, This is his E-mail address: mehdi.0.saadi@gmail.com'),
+(5, 'thank you|thank u|thanks', 'I am here to help.'),
+(6, 'awesome|you are awesome|wow|wow you are awesome', 'Thank you so much.'),
+(7, 'by|bye|bye bye|by by|goodby|goodbye', 'See you soon.');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `response`
+--
+ALTER TABLE `response`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `response`
+--
+ALTER TABLE `response`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
